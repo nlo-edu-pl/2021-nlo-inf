1ad8cf1584d8eee23148419a3f3c2f7173025fe3
Mateusz Adamowski <mateusz.adamowski@nlo.edu.pl>
2021-10-07 15:50:43 +0200

    przykład przed kartkówką

---
+++ 2021-09-30/zadanie2-4.py
@@ -0,0 +1,22 @@
+# Napisać funkcję, która dla podanych długości boków prostokąta oraz długości odcinka, zwraca informację, czy dany odcinek zmieściłby się wewnątrz prostokąta.
+
+def czy_odcinek_w_prostokacie(a, b, odc):
+    ...
+
+# a - float
+# b - float
+# odc - float
+#
+# zwraca: bool
+
+
+def czy_odcinek_w_prostokacie(bok_a: float, bok_b: float, odc: float) -> bool:
+    ...
+
+
+# int
+# float
+# bool
+# str 
+# list []
+# dict {}
