50784efa5b30e647828b348bab2da23e57f62ab9
Mateusz Adamowski <mateusz.adamowski@nlo.edu.pl>
2021-10-28 16:25:08 +0200

    dhondt

--- 2021-10-28/dhondt.py
+++ 2021-10-28/dhondt.py
@@ -1,30 +1,44 @@
-# import os
-# os.chdir(os.path.dirname(__file__))
-
-mandaty = 7
-
-with open("2021-10-21/wybory1.txt") as f:
-    for linia in f:
-        komitet, glosy = linia.split(":")
-        glosy = int(glosy)
-        print(komitet)
-        print(glosy)
-
-
-# partia ABC = 199999
-# partia ... = ....
-
 def ilorazy(liczba_mandatow: int, liczba_glosow: int) -> list:
     wynik = []
     for i in range(1, liczba_mandatow + 1):
         wynik.append(liczba_glosow // i)
     return wynik
 
-assert ilorazy(0, 500) == []
-assert ilorazy(1, 700) == [700]
-assert ilorazy(3, 3000) == [3000, 1500, 1000]
+def ilorazy_komitetu(komitet: str, ilorazy: list) -> list:
+    wynik = []
+    for iloraz in ilorazy:
+        krotka = (iloraz, komitet)
+        wynik.append(krotka)
+    return wynik
 
-print(ilorazy(4, 1000))
-assert ilorazy(4, 1000) == [1000, 500, 333, 250]
+def zlicz_ilorazy(ilorazy: list) -> dict:
+    wynik = {}
+    for iloraz, komitet in ilorazy:
+        if komitet in wynik:
+            wynik[komitet] = wynik[komitet] + 1
+        else:
+            wynik[komitet] = 1
+    return wynik
 
-print("ok")
+# import os
+# os.chdir(os.path.dirname(__file__))
+
+mandaty = 5
+
+wszystkie = []
+
+with open("2021-10-28/wybory1.txt") as f:
+    for linia in f:
+        komitet, glosy = linia.split(":")
+        glosy = int(glosy)
+        print(komitet)
+        i = ilorazy(mandaty, glosy)
+        wszystkie = wszystkie + ilorazy_komitetu(komitet, i)
+        #print(ilorazy_komitetu(komitet, i))
+
+wszystkie.sort(reverse=True)
+biorace = wszystkie[:mandaty]
+ile_mandatow = zlicz_ilorazy(biorace)
+
+print(ile_mandatow)
+
