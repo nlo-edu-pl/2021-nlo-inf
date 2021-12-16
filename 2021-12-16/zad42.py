bb2c394ea231cefb4e1bd4eee156a8307d561533
Mateusz Adamowski <mateusz.adamowski@nlo.edu.pl>
2021-12-16 16:04:39 +0100

    zadanie 4.2

--- 2021-12-16/zad42.py
+++ 2021-12-16/zad42.py
@@ -1,2 +1,11 @@
 import zad4
+lw =0
+nazwapliku = "2021-12-16/liczby.txt"
 
+with open(nazwapliku) as f:
+    for linia in f:
+        liczba = int(linia)
+        # print(liczba) 
+        if zad4.czy_wesola(liczba):
+            lw = lw + 1
+print(lw)
