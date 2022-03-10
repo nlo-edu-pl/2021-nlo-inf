e4ccdda089d756ee99cb252ad9b9469366057223
Mateusz Adamowski <mateusz.adamowski@nlo.edu.pl>
2022-03-10 15:02:30 +0100

    sortowanie przez zliczanie

--- 2022-03-10/zadanie1.py
+++ 2022-03-10/zadanie1.py
@@ -1,6 +1,9 @@
 def counting_sort(liczby: list) -> list:
     """Sortowanie przez zliczanie liczb od 0 do 99"""
-    ...
+    wynik = []
+    for wartosc, ile_powt in enumerate(count_values(liczby)):
+        wynik.extend([wartosc] * ile_powt)
+    return wynik
 
 def count_values(liczby: list) -> list:
     """Policz wystąpienia liczb"""
@@ -9,9 +12,22 @@ def count_values(liczby: list) -> list:
         licznik[x] += 1
     return licznik
 
+
+def counting_sort2(liczby: list) -> list:
+    """Sortowanie przez zliczanie liczb od 0 do 99"""
+    licznik = [0] * 100
+    for x in liczby:
+        licznik[x] += 1
+    wynik = []
+    for wartosc, ile_powt in enumerate(licznik):
+        wynik.extend([wartosc] * ile_powt)
+    return wynik
+
 liczby = [5, 7, 11, 3, 3, 5, 9, 10, 34, 56, 56, 5, 7]
 
 
 print(count_values(liczby))
 
-# assert counting_sort(liczby) == [3, 3, 5, 5, 5, 7, 7, 9, 10, 11, 34, 56, 56]
+print(counting_sort(liczby))
+
+assert counting_sort(liczby) == [3, 3, 5, 5, 5, 7, 7, 9, 10, 11, 34, 56, 56]
