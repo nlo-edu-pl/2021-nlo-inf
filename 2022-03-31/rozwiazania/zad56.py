0b9113fb427f38dbbd86f1329b99ee3e8933aa59
Mateusz Adamowski <mateusz.adamowski@nlo.edu.pl>
2022-04-07 13:11:17 +0200

    brakujący wykaz

--- 2022-03-31/rozwiazania/zad56.py
+++ 2022-03-31/rozwiazania/zad56.py
@@ -2,7 +2,7 @@ import sqlite3
 
 db = sqlite3.connect('zad5.db')
 
-print("Zad 5.6")
+print("Zad 5.6 najwięcej")
 sql = """
     Select
         godz - 7,
@@ -16,6 +16,22 @@ sql = """
     Group By godz
     Order By godz Desc
     Limit 1;
+"""
+
+for r in db.execute(sql):
+    print(r)
+print("Zad 5.6 wszystkie")
+sql = """
+    Select
+        godz - 7,
+        count(*)
+    From (
+        Select
+            GODZINAZASZCZEPIENIA,
+            Substr(GODZINAZASZCZEPIENIA, 1, 2) as godz
+        From punktszczepien
+    ) As x
+    Group By godz;
 """
 
 for r in db.execute(sql):
