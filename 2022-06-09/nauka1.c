47807a3864fcb7a571ee07c1e8cf948da5c1a910
Mateusz Adamowski <mateusz.adamowski@nlo.edu.pl>
2022-06-09 15:26:56 +0200

    C - podstawy

---
+++ 2022-06-09/nauka1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int a, b;
+
+    a = 74;
+    b = 10;
+
+    // print("a wynosi {a}")
+    printf("a wynosi %d a b = %d \n", a, b);
+    printf("a wynosi %x\n", a);
+
+
+}
+
